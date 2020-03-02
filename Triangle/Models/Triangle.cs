19ffb97ea12d10d0e2a58f406b42bf72eb4d485e
Keturah Howard <keturah.dev@gmail.com>
2020-03-02 11:44:30 -0800

    slightly modify first two specs, add minimal code to pass first spec

--- Triangle/Models/Triangle.cs
+++ Triangle/Models/Triangle.cs
@@ -22,7 +22,14 @@ namespace Triangle
     //baby methods
     public static bool isTriangle (int side1, int side2, int side3)
     {
-      return false; 
+      if (((side1 + side2) < side3 || (side2 + side3) < side1 || (side1 + side3) < side2))
+      {
+        return false;     
+      }
+      else
+      {
+        return true;
+      }
     }
 
     public static void Main ()
